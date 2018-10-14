b71e727c9944149180cff2288881275f6ec4d05e
Thanawat Lukuan <thanawat_lukuan@cmu.ac.th>
2018-10-14 15:31:15 +0700

    Adding converter

--- PEHS_MobileApplication/app/src/main/java/com/yangzxcc/macintoshhd/api/ApiClient.java
+++ PEHS_MobileApplication/app/src/main/java/com/yangzxcc/macintoshhd/api/ApiClient.java
@@ -1,5 +1,8 @@
 package com.yangzxcc.macintoshhd.api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,12 +11,20 @@ public class ApiClient {
     public static final String BASE_URL = "http://10.0.2.2:8000/api/auth/";
 //    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
     public static Retrofit retrofit = null;
+    public static Gson gson = null;
+
+
+
 
     public static Retrofit getRetrofit(){
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
         if (retrofit == null){
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
