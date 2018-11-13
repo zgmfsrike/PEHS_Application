0f0f79f8f2421c67c857ae4dad7a06632a3f3f2d
nuBoreZ <zgmfstike@gmail.com>
2018-11-14 00:03:06 +0700

    update jumbo plus ip

--- PEHS_MobileApplication/app/src/main/java/com/yangzxcc/macintoshhd/api/ApiClient.java
+++ PEHS_MobileApplication/app/src/main/java/com/yangzxcc/macintoshhd/api/ApiClient.java
@@ -15,24 +15,24 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-//    public static final String BASE_URL = "http://127.0.0.1:8000/api/auth/";
-    public static final String BASE_URL = "http://10.0.2.2:8000/api/auth/";
+    //    public static final String BASE_URL = "http://192.168.1.3:8000/api/auth/";
+    //jumbo ip
+    public static final String BASE_URL = "http://10.80.43.133:8000/api/auth/";
+    //    public static final String BASE_URL = "http://10.0.2.2:8000/api/auth/";
     public static final String BASE__MOCK_URL = "https://api.myjson.com";
-//    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    //    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
     public static Retrofit retrofit = null;
     public static Gson gson = null;
 
 
-
-
-    public static Retrofit getRetrofit(){
+    public static Retrofit getRetrofit() {
 
         gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
                 .setLenient()
                 .create();
 
-        if (retrofit == null){
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create(gson))
@@ -41,13 +41,13 @@ public class ApiClient {
         return retrofit;
     }
 
-    public static Retrofit getMockUpRetrofit(){
+    public static Retrofit getMockUpRetrofit() {
         gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
                 .setLenient()
                 .create();
 
-        if (retrofit == null){
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE__MOCK_URL)
                     .addConverterFactory(GsonConverterFactory.create(gson))
@@ -56,13 +56,13 @@ public class ApiClient {
         return retrofit;
     }
 
-    public static Retrofit getMock(){
+    public static Retrofit getMock() {
         gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
                 .setLenient()
                 .create();
 
-        if (retrofit == null){
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE__MOCK_URL)
                     .addConverterFactory(GsonConverterFactory.create(gson))
